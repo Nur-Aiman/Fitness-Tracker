9b5a15624988cd233cd82a381e1b39955c99ed09
Nur-Aiman <64392281+Nur-Aiman@users.noreply.github.com>
2024-02-18 11:50:20 +0800

    update cors

--- ft_be/app.js
+++ ft_be/app.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 
 var corsOptions = {
   origin: function (origin, callback) {
-    const allowedOrigins = ['http://localhost:3000', 'https://fitness-tracker-api-bi2u.onrender.com'];
+    const allowedOrigins = ['http://localhost:3000', 'https://fitness-tracker-9qve.onrender.com'];
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
